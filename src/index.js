ad8d78371a7a804750d882ac471dcba81a287755
Fajrendajs <40194093+Fajrendajs@users.noreply.github.com>
2018-10-10 10:10:36 +0200

    testiranje

--- src/index.js
+++ src/index.js
@@ -13,6 +13,61 @@ const user = {
 };
 const name = " Sahalin";
 const element = <h1>Hello, {formatname(user)}</h1>;
+/*
+function tick() {
+  const time = (
+    <div>
+      <h1>hello, world</h1>
+      <h2>It is {new Date().toLocaleTimeString()} </h2>
+      <h2>It is {new Date().toLocaleDateString()} </h2>
+    </div>
+  );
+  ReactDOM.render(time, rootElement);
+}
+setInterval(tick, 1000);
+*/
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+
+function Avatar(props) {
+  return (
+    <img className="Avatar" src={props.user.avatarUrl} alt={props.user.name} />
+  );
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">{props.user.name}</div>
+    </div>
+  );
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-date">{formatDate(props.date)}</div>
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text: "I hope you enjoy learning React!",
+  author: {
+    name: "Hello Kitty",
+    avatarUrl: "https://placekitten.com/g/64/64"
+  }
+};
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(element, rootElement);
+ReactDOM.render(
+  <Comment date={comment.date} text={comment.text} author={comment.author} />,
+  rootElement
+);
+
+//setInterval(tick, 1000);
